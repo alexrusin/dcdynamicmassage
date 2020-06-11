f808bf7fc21c456c270c13efeb6d9db913ec4155
Alex Rusin <alex@sunriseintegration.com>
2020-06-11 07:29:14 -0700

    add social links

--- src/resources/js/components/Navbar.vue
+++ src/resources/js/components/Navbar.vue
@@ -1,14 +1,8 @@
 <template>
   <header>
     <div class="top-strip flex justify-between bg-blue-400">
-      <div
-        class="px-6 py-4 sm:w-3/4 sm:mx-auto text-gray-300 flex items-center space-x-2"
-      >
-        <svg
-          class="h-4 w-4 fill-current"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-        >
+      <div class="px-6 py-4 sm:w-3/4 sm:mx-auto text-gray-300 flex items-center space-x-2">
+        <svg class="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
           <path
             d="M17.256 12.253c-.096-.667-.611-1.187-1.274-1.342-2.577-.604-3.223-2.088-3.332-3.734C12.193 7.092 11.38 7 10 7s-2.193.092-2.65.177c-.109 1.646-.755 3.13-3.332 3.734-.663.156-1.178.675-1.274 1.342l-.497 3.442C2.072 16.907 2.962 18 4.2 18h11.6c1.237 0 2.128-1.093 1.953-2.305l-.497-3.442zM10 15.492c-1.395 0-2.526-1.12-2.526-2.5s1.131-2.5 2.526-2.5 2.526 1.12 2.526 2.5-1.132 2.5-2.526 2.5zM19.95 6c-.024-1.5-3.842-3.999-9.95-4C3.891 2.001.073 4.5.05 6s.021 3.452 2.535 3.127c2.941-.381 2.76-1.408 2.76-2.876C5.345 5.227 7.737 4.98 10 4.98s4.654.247 4.655 1.271c0 1.468-.181 2.495 2.76 2.876C19.928 9.452 19.973 7.5 19.95 6z"
           />
@@ -16,75 +10,166 @@
         <div class="font-semibold">818-257-9496</div>
       </div>
       <div class="flex sm:hidden items-center space-x-4 px-6">
-            <div>
-                <svg class="h-4 w-4 text-gray-300 hover:text-gray-100 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M17 1H3c-1.1 0-2 .9-2 2v14c0 1.101.9 2 2 2h7v-7H8V9.525h2v-2.05c0-2.164 1.212-3.684 3.766-3.684l1.803.002v2.605h-1.197c-.994 0-1.372.746-1.372 1.438v1.69h2.568L15 12h-2v7h4c1.1 0 2-.899 2-2V3c0-1.1-.9-2-2-2z"/></svg>
-            </div>
-            <div>
-                <svg class="h-4 w-4 text-gray-300 hover:text-gray-100 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M17.316 6.246c.008.162.011.326.011.488 0 4.99-3.797 10.742-10.74 10.742-2.133 0-4.116-.625-5.787-1.697a7.577 7.577 0 0 0 5.588-1.562 3.779 3.779 0 0 1-3.526-2.621 3.858 3.858 0 0 0 1.705-.065 3.779 3.779 0 0 1-3.028-3.703v-.047a3.766 3.766 0 0 0 1.71.473 3.775 3.775 0 0 1-1.168-5.041 10.716 10.716 0 0 0 7.781 3.945 3.813 3.813 0 0 1-.097-.861 3.773 3.773 0 0 1 3.774-3.773 3.77 3.77 0 0 1 2.756 1.191 7.602 7.602 0 0 0 2.397-.916 3.789 3.789 0 0 1-1.66 2.088 7.55 7.55 0 0 0 2.168-.594 7.623 7.623 0 0 1-1.884 1.953z"/></svg>
-            </div>
-            <div>
-                <svg class="h-4 w-4 text-gray-300 hover:text-gray-100 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.538 12.471l4.523 1.466s.647.119.64.552c-.004.305-.197.652-.197.652l-1.91 2.756s-.341.286-.686.286c-.344 0-.741-.537-.741-.537l-2.417-4.073s-.272-.594.05-.921c.295-.3.738-.181.738-.181zM11.57 10.6c.231.396.87.281.87.281l4.513-1.331s.615-.253.703-.589c.086-.337-.102-.743-.102-.743l-2.157-2.564s-.187-.324-.575-.357c-.428-.037-.691.486-.691.486l-2.55 4.05c.001-.001-.224.402-.011.767zM9.438 9.021c.531-.132.616-.911.616-.911l-.036-6.485s-.08-.8-.436-1.017c-.559-.342-.724-.164-.884-.14L4.951 1.873s-.367.123-.558.432c-.273.437.277 1.079.277 1.079l3.894 5.358s.385.401.874.279zm-.925 2.624c.013-.5-.595-.801-.595-.801L3.89 8.791s-.597-.248-.887-.075c-.221.132-.418.372-.437.583l-.262 3.259s-.039.565.106.822c.205.364.881.111.881.111l4.702-1.049c.182-.124.502-.136.52-.797zm1.169 1.759c-.404-.209-.887.224-.887.224l-3.148 3.498s-.393.535-.293.863c.094.308.25.461.47.569l3.162 1.007s.383.08.674-.005c.412-.121.336-.772.336-.772l.071-4.736c0 .001-.016-.455-.385-.648z"/></svg>
-            </div>
+        <div>
+          <a href="https://www.facebook.com/dcdynamicmassage" target="_blank">
+            <svg
+              class="h-4 w-4 text-gray-300 hover:text-gray-100 fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path
+                d="M17 1H3c-1.1 0-2 .9-2 2v14c0 1.101.9 2 2 2h7v-7H8V9.525h2v-2.05c0-2.164 1.212-3.684 3.766-3.684l1.803.002v2.605h-1.197c-.994 0-1.372.746-1.372 1.438v1.69h2.568L15 12h-2v7h4c1.1 0 2-.899 2-2V3c0-1.1-.9-2-2-2z"
+              />
+            </svg>
+          </a>
         </div>
+        <div>
+          <a href="https://twitter.com/ddynamicmassage" target="_blank">
+            <svg
+              class="h-4 w-4 text-gray-300 hover:text-gray-100 fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path
+                d="M17.316 6.246c.008.162.011.326.011.488 0 4.99-3.797 10.742-10.74 10.742-2.133 0-4.116-.625-5.787-1.697a7.577 7.577 0 0 0 5.588-1.562 3.779 3.779 0 0 1-3.526-2.621 3.858 3.858 0 0 0 1.705-.065 3.779 3.779 0 0 1-3.028-3.703v-.047a3.766 3.766 0 0 0 1.71.473 3.775 3.775 0 0 1-1.168-5.041 10.716 10.716 0 0 0 7.781 3.945 3.813 3.813 0 0 1-.097-.861 3.773 3.773 0 0 1 3.774-3.773 3.77 3.77 0 0 1 2.756 1.191 7.602 7.602 0 0 0 2.397-.916 3.789 3.789 0 0 1-1.66 2.088 7.55 7.55 0 0 0 2.168-.594 7.623 7.623 0 0 1-1.884 1.953z"
+              />
+            </svg>
+          </a>
+        </div>
+        <div>
+          <a href="https://www.yelp.com/biz/dc-dynamic-massage-woodland-hills" target="_blank">
+            <svg
+              class="h-4 w-4 text-gray-300 hover:text-gray-100 fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path
+                d="M12.538 12.471l4.523 1.466s.647.119.64.552c-.004.305-.197.652-.197.652l-1.91 2.756s-.341.286-.686.286c-.344 0-.741-.537-.741-.537l-2.417-4.073s-.272-.594.05-.921c.295-.3.738-.181.738-.181zM11.57 10.6c.231.396.87.281.87.281l4.513-1.331s.615-.253.703-.589c.086-.337-.102-.743-.102-.743l-2.157-2.564s-.187-.324-.575-.357c-.428-.037-.691.486-.691.486l-2.55 4.05c.001-.001-.224.402-.011.767zM9.438 9.021c.531-.132.616-.911.616-.911l-.036-6.485s-.08-.8-.436-1.017c-.559-.342-.724-.164-.884-.14L4.951 1.873s-.367.123-.558.432c-.273.437.277 1.079.277 1.079l3.894 5.358s.385.401.874.279zm-.925 2.624c.013-.5-.595-.801-.595-.801L3.89 8.791s-.597-.248-.887-.075c-.221.132-.418.372-.437.583l-.262 3.259s-.039.565.106.822c.205.364.881.111.881.111l4.702-1.049c.182-.124.502-.136.52-.797zm1.169 1.759c-.404-.209-.887.224-.887.224l-3.148 3.498s-.393.535-.293.863c.094.308.25.461.47.569l3.162 1.007s.383.08.674-.005c.412-.121.336-.772.336-.772l.071-4.736c0 .001-.016-.455-.385-.648z"
+              />
+            </svg>
+          </a>
+        </div>
+      </div>
     </div>
-    <div class="logo-strip px-6 py-8 sm:w-3/4 sm:mx-auto flex justify-around sm:justify-between items-baseline">
-        <div class="flex items-center">
-            <div class="-ml-1 mr-1">
-                <img class="h-8 sm:h-10" src="/img/logo.png" alt="DC Dynamic Massage">
-            </div>
-            <div>
-                <p class="logo-text font-bold text-2xl sm:text-3xl">DC Dynamic Massage</p>
-            </div>
+    <div
+      class="logo-strip px-6 py-8 sm:w-3/4 sm:mx-auto flex justify-around sm:justify-between items-baseline"
+    >
+      <div class="flex items-center">
+        <div class="-ml-1 mr-1">
+          <img class="h-8 sm:h-10" src="/img/logo.png" alt="DC Dynamic Massage" />
         </div>
-        <div class="hidden sm:flex items-center space-x-2 md:space-x-6">
-            <div>
-                <svg class="h-6 w-6 text-blue-600 hover:text-blue-400 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M17 1H3c-1.1 0-2 .9-2 2v14c0 1.101.9 2 2 2h7v-7H8V9.525h2v-2.05c0-2.164 1.212-3.684 3.766-3.684l1.803.002v2.605h-1.197c-.994 0-1.372.746-1.372 1.438v1.69h2.568L15 12h-2v7h4c1.1 0 2-.899 2-2V3c0-1.1-.9-2-2-2z"/></svg>
-            </div>
-            <div>
-                <svg class="h-6 w-6 text-blue-400 hover:text-blue-200 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M17.316 6.246c.008.162.011.326.011.488 0 4.99-3.797 10.742-10.74 10.742-2.133 0-4.116-.625-5.787-1.697a7.577 7.577 0 0 0 5.588-1.562 3.779 3.779 0 0 1-3.526-2.621 3.858 3.858 0 0 0 1.705-.065 3.779 3.779 0 0 1-3.028-3.703v-.047a3.766 3.766 0 0 0 1.71.473 3.775 3.775 0 0 1-1.168-5.041 10.716 10.716 0 0 0 7.781 3.945 3.813 3.813 0 0 1-.097-.861 3.773 3.773 0 0 1 3.774-3.773 3.77 3.77 0 0 1 2.756 1.191 7.602 7.602 0 0 0 2.397-.916 3.789 3.789 0 0 1-1.66 2.088 7.55 7.55 0 0 0 2.168-.594 7.623 7.623 0 0 1-1.884 1.953z"/></svg>
-            </div>
-            <div>
-                <svg class="h-6 w-6 text-red-600 hover:text-red-500 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.538 12.471l4.523 1.466s.647.119.64.552c-.004.305-.197.652-.197.652l-1.91 2.756s-.341.286-.686.286c-.344 0-.741-.537-.741-.537l-2.417-4.073s-.272-.594.05-.921c.295-.3.738-.181.738-.181zM11.57 10.6c.231.396.87.281.87.281l4.513-1.331s.615-.253.703-.589c.086-.337-.102-.743-.102-.743l-2.157-2.564s-.187-.324-.575-.357c-.428-.037-.691.486-.691.486l-2.55 4.05c.001-.001-.224.402-.011.767zM9.438 9.021c.531-.132.616-.911.616-.911l-.036-6.485s-.08-.8-.436-1.017c-.559-.342-.724-.164-.884-.14L4.951 1.873s-.367.123-.558.432c-.273.437.277 1.079.277 1.079l3.894 5.358s.385.401.874.279zm-.925 2.624c.013-.5-.595-.801-.595-.801L3.89 8.791s-.597-.248-.887-.075c-.221.132-.418.372-.437.583l-.262 3.259s-.039.565.106.822c.205.364.881.111.881.111l4.702-1.049c.182-.124.502-.136.52-.797zm1.169 1.759c-.404-.209-.887.224-.887.224l-3.148 3.498s-.393.535-.293.863c.094.308.25.461.47.569l3.162 1.007s.383.08.674-.005c.412-.121.336-.772.336-.772l.071-4.736c0 .001-.016-.455-.385-.648z"/></svg>
-            </div>
+        <div>
+          <p class="logo-text font-bold text-2xl sm:text-3xl">DC Dynamic Massage</p>
         </div>
+      </div>
+      <div class="hidden sm:flex items-center space-x-2 md:space-x-6">
+        <div>
+          <a href="https://www.facebook.com/dcdynamicmassage" target="_blank">
+            <svg
+              class="h-6 w-6 text-blue-600 hover:text-blue-400 fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path
+                d="M17 1H3c-1.1 0-2 .9-2 2v14c0 1.101.9 2 2 2h7v-7H8V9.525h2v-2.05c0-2.164 1.212-3.684 3.766-3.684l1.803.002v2.605h-1.197c-.994 0-1.372.746-1.372 1.438v1.69h2.568L15 12h-2v7h4c1.1 0 2-.899 2-2V3c0-1.1-.9-2-2-2z"
+              />
+            </svg>
+          </a>
+        </div>
+        <div>
+          <a href="https://twitter.com/ddynamicmassage" target="_blank">
+            <svg
+              class="h-6 w-6 text-blue-400 hover:text-blue-200 fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path
+                d="M17.316 6.246c.008.162.011.326.011.488 0 4.99-3.797 10.742-10.74 10.742-2.133 0-4.116-.625-5.787-1.697a7.577 7.577 0 0 0 5.588-1.562 3.779 3.779 0 0 1-3.526-2.621 3.858 3.858 0 0 0 1.705-.065 3.779 3.779 0 0 1-3.028-3.703v-.047a3.766 3.766 0 0 0 1.71.473 3.775 3.775 0 0 1-1.168-5.041 10.716 10.716 0 0 0 7.781 3.945 3.813 3.813 0 0 1-.097-.861 3.773 3.773 0 0 1 3.774-3.773 3.77 3.77 0 0 1 2.756 1.191 7.602 7.602 0 0 0 2.397-.916 3.789 3.789 0 0 1-1.66 2.088 7.55 7.55 0 0 0 2.168-.594 7.623 7.623 0 0 1-1.884 1.953z"
+              />
+            </svg>
+          </a>
+        </div>
+        <div>
+          <a href="https://www.yelp.com/biz/dc-dynamic-massage-woodland-hills" target="_blank">
+            <svg
+              class="h-6 w-6 text-red-600 hover:text-red-500 fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path
+                d="M12.538 12.471l4.523 1.466s.647.119.64.552c-.004.305-.197.652-.197.652l-1.91 2.756s-.341.286-.686.286c-.344 0-.741-.537-.741-.537l-2.417-4.073s-.272-.594.05-.921c.295-.3.738-.181.738-.181zM11.57 10.6c.231.396.87.281.87.281l4.513-1.331s.615-.253.703-.589c.086-.337-.102-.743-.102-.743l-2.157-2.564s-.187-.324-.575-.357c-.428-.037-.691.486-.691.486l-2.55 4.05c.001-.001-.224.402-.011.767zM9.438 9.021c.531-.132.616-.911.616-.911l-.036-6.485s-.08-.8-.436-1.017c-.559-.342-.724-.164-.884-.14L4.951 1.873s-.367.123-.558.432c-.273.437.277 1.079.277 1.079l3.894 5.358s.385.401.874.279zm-.925 2.624c.013-.5-.595-.801-.595-.801L3.89 8.791s-.597-.248-.887-.075c-.221.132-.418.372-.437.583l-.262 3.259s-.039.565.106.822c.205.364.881.111.881.111l4.702-1.049c.182-.124.502-.136.52-.797zm1.169 1.759c-.404-.209-.887.224-.887.224l-3.148 3.498s-.393.535-.293.863c.094.308.25.461.47.569l3.162 1.007s.383.08.674-.005c.412-.121.336-.772.336-.772l.071-4.736c0 .001-.016-.455-.385-.648z"
+              />
+            </svg>
+          </a>
+        </div>
+      </div>
     </div>
     <div class="menu-strip bg-blue-400">
-        <div class="sm:hidden py-3 flex justify-end px-6">
-                <button @click="isOpen = !isOpen" type="button" class="text-gray-300 hover:text-white focus:text-white focus:outline-none">
-                    <svg class="h-8 w-8 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24"
-                        height="24">
-                        <path class="heroicon-ui"
-                            d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z" />
-                        </svg>
-                </button>
-            </div>
-        <div :class="isOpen ? '' : 'hidden'" class="sm:flex sm:items-center-mt-4 px-6 py-4 -my-6 sm:my-0 sm:mt-0 md:text-xl sm:w-1/2 sm:mx-auto sm:justify-around">
-            <a href="/" :class="urlPath == '' ? 'bg-gray-700' : ''" class="block text-white font-semibold rounded px-2 py-1 hover:bg-gray-700">Home</a>
-            <a href="/about" :class="urlPath == '/about' ? 'bg-gray-700' : ''"class="block text-white font-semibold rounded px-2 py-1 mt-1 hover:bg-gray-700 sm:mt-0">About</a>
-            <a href="/services" :class="urlPath == '/services' ? 'bg-gray-700' : '/services'" class="block text-white font-semibold rounded px-2 py-1 mt-1 hover:bg-gray-700 sm:mt-0">Services</a>
-            <a href="/contact" :class="urlPath == '/contact' ? 'bg-gray-700' : '/contact'" class="block text-white font-semibold rounded px-2 py-1 mt-1 hover:bg-gray-700 sm:mt-0">Contact</a>
-        </div>
+      <div class="sm:hidden py-3 flex justify-end px-6">
+        <button
+          @click="isOpen = !isOpen"
+          type="button"
+          class="text-gray-300 hover:text-white focus:text-white focus:outline-none"
+        >
+          <svg
+            class="h-8 w-8 fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+          >
+            <path
+              class="heroicon-ui"
+              d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+            />
+          </svg>
+        </button>
+      </div>
+      <div
+        :class="isOpen ? '' : 'hidden'"
+        class="sm:flex sm:items-center-mt-4 px-6 py-4 -my-6 sm:my-0 sm:mt-0 md:text-xl sm:w-1/2 sm:mx-auto sm:justify-around"
+      >
+        <a
+          href="/"
+          :class="urlPath == '' ? 'bg-gray-700' : ''"
+          class="block text-white font-semibold rounded px-2 py-1 hover:bg-gray-700"
+        >Home</a>
+        <a
+          href="/about"
+          :class="urlPath == '/about' ? 'bg-gray-700' : ''"
+          class="block text-white font-semibold rounded px-2 py-1 mt-1 hover:bg-gray-700 sm:mt-0"
+        >About</a>
+        <a
+          href="/services"
+          :class="urlPath == '/services' ? 'bg-gray-700' : '/services'"
+          class="block text-white font-semibold rounded px-2 py-1 mt-1 hover:bg-gray-700 sm:mt-0"
+        >Services</a>
+        <a
+          href="/contact"
+          :class="urlPath == '/contact' ? 'bg-gray-700' : '/contact'"
+          class="block text-white font-semibold rounded px-2 py-1 mt-1 hover:bg-gray-700 sm:mt-0"
+        >Contact</a>
+      </div>
     </div>
   </header>
 </template>
 
 <script>
 export default {
-
-    computed: {
-        urlPath() {
-            return window.location.pathname.replace(/\/$/, "");
-        }     
-    },
+  computed: {
+    urlPath() {
+      return window.location.pathname.replace(/\/$/, "");
+    }
+  },
   data() {
     return {
-      isOpen: false,
+      isOpen: false
     };
-  },
+  }
 };
 </script>
 <style scoped>
-    .logo-text {
-        color: #74d0e1;
-    }
+.logo-text {
+  color: #74d0e1;
+}
 </style>
