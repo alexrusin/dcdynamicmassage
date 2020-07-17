de611d39e4531df8806b742c8825f811af8c22ba
Maryna <m.dymovich@gmail.com>
2020-07-17 10:50:30 +0900

    Created a reviews section in index.hbs and Yelp reviews template in vue

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -5,6 +5,7 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 Vue.component('navbar', require('./components/Navbar.vue').default);
 Vue.component('coupons', require('./components/Coupons.vue').default);
 Vue.component('current-year', require('./components/CurrentYear.vue').default);
+Vue.component('yelp-reviews', require('./components/YelpReviews.vue').default);
 
 const app = new Vue({
     el: '#app',
---
+++ src/resources/js/components/YelpReview.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="reviews-card bg-white border-2 border-red-600 rounded-lg shadow-lg mt-8">
+        <div class="px-4">
+            <img class="w-5/6 object-center mb-4 static" src="/img/yelp-logo.jpg" alt="Yelp" />  
+                <div class="w-12 h-12 inline-block">         
+                    <img class="w-full h-full pl-1 rounded-full" :src="data.user.image_url" :alt="data.user.profile_url"> 
+                </div>
+                <div class="w-2/3 inline-block align-top">
+                    <p class="pl-2 inline-block font-bold break-normal text-sm">{{data.user.name}}</p>
+                    <p class="pl-2 text-xs">{{data.time_created}}</p>
+                </div>
+        </div>
+        <div class="px-4 pb-4">
+            <img class="w-1/2" src="/img/stars_placeholder.jpg"> 
+            <p class="text-justify text-xs">{{data.text}}</p>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        data: {
+            type: Object,
+            required: true
+        },
+    }
+    
+}
+</script>
