20e7c8d95cd8c9d7a5ba088cf41210bc2e9cf9eb
Alex Rusin <alexrusin_2000@yahoo.com>
2020-07-22 08:51:33 -0700

    wip on coupons

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -4,6 +4,7 @@ window.Vue = require('vue');
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('navbar', require('./components/Navbar.vue').default);
 Vue.component('coupons', require('./components/Coupons.vue').default);
+Vue.component('coupon', require('./components/Coupon.vue').default);
 Vue.component('current-year', require('./components/CurrentYear.vue').default);
 Vue.component('yelp-reviews', require('./components/YelpReviews.vue').default);
 Vue.component('contact-form', require('./components/ContactForm.vue').default);
